7ce81be828b7c79999899b4d497a44331169b470
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-03-27 20:13:08 +0900

    サンプルページを追加

---
+++ src/components/ModernFeatures/index.tsx
@@ -0,0 +1,28 @@
+const ModernFeatures = () => {
+  return (
+    <section className="relative mt-48">
+      <div className="relative">
+        <Background />
+      </div>
+    </section>
+  )
+}
+
+const Background = () => {
+  return (
+    <div className="absolute inset-x-0 top-40">
+      <div className="absolute inset-x-0 top-0 block h-[37.5rem] bg-gradient-to-b from-[#0c1120] " />
+      <div
+        className="absolute  inset-x-0 top-0 bg-top bg-no-repeat"
+        style={{
+          backgroundImage: 'url(/0.png)',
+          backgroundSize: '125.5rem 100%',
+          height: '28.8125rem',
+        }}
+      />
+      <div className="bg-grid-slate-900/[0.04] bg-grid-slate-100/[0.03] absolute inset-x-0 top-0 h-[37.5rem] border-t border-slate-100/5 bg-top bg-[center_top_-1px] [mask-image:linear-gradient(0deg,transparent,black)] " />
+    </div>
+  )
+}
+
+export default ModernFeatures
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,22 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '@/components/Header'
+import ModernFeatures from '@/components/ModernFeatures'
+import NotComponents from '@/components/NotComponents'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div className="flex flex-col items-center">
-          <a
-            href="#"
-            className="text-black-800 mb-8 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl"
-            aria-label="logo"
-          >
-            <svg
-              width="95"
-              height="94"
-              viewBox="0 0 95 94"
-              className="h-auto w-6 text-indigo-500"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-            </svg>
-            Flowrift
-          </a>
-          <h1 className="mb-2 text-center text-2xl font-bold text-gray-800 md:text-3xl">
-            Hello World
-          </h1>
-        </div>
-      </div>
-    </div>
+    <>
+      <main className="min-h-screen bg-slate-900 text-slate-400 antialiased">
+        <Head>
+          <title>Not Tailwind CSS | Code Hike Demo</title>
+        </Head>
+
+        <Header />
+        <ModernFeatures />
+        <NotComponents />
+      </main>
+    </>
   )
 }
 
